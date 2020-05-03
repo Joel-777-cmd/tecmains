422366a912d0ec7a28fa52ad0e5395946c4d1d58
Joel-777-cmd <64746637+Joel-777-cmd@users.noreply.github.com>
2020-05-03 19:11:35 -0400

    tecmains

---
+++ global.php
@@ -0,0 +1,21 @@
+﻿<?php
+    //IP DEL SERVIDOR DE BASE DE DATOS
+    define("DB_HOST","localhost");
+
+    //NOMBRE DE LA BASE DE DATOS
+    define("DB_NAME","ottos_25426197_dbsistema");
+
+    //USUARIO DE LA BASE DE DATOS
+    define("DB_USERNAME","ottos_25426197");
+
+    //CONTRASEÑA DEL USUARIO DE LA BASE DE DATOS
+    define("DB_PASSWORD","");
+
+    //CODIFICACION DE LOS CARACTERES
+    define("DB_ENCODE","utf8");
+
+    //CONSTANTE COMO NOMBRE DEL PROYECTO
+    define("PRO_NOMBRE","ITVentas");
+
+    
+?>
